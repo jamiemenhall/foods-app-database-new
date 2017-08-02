d4c39c688809828adb40a75f2935941438a0f455
jamiemenhall <jamiemenhall@mac.com>
2017-08-02 14:05:06 -0400

    updated scrape.py

--- app/scrape.py
+++ app/scrape.py
@@ -1,29 +1,22 @@
-import json
-import requests
-import time
-
-from instance import config
-
 def get_db_status(initial_search_request):
     '''
     initial_search_request = 'https://api.nal.usda.gov/ndb/search/?format=json&q=&sort=n&max=1&offset=0&api_key=DEMO_KEY'
-    total_number = total number of items in usda database
-    current_sr = Standard Release version of the data being reported
+    current_total = total number of items in usda database at the time of request
+    current_sr = Standard Release version of the data at the time of request
 
-    Returns {'total': total_number, 'sr': current_sr}
+    Returns {'current_total': current_total, 'current_sr': current_sr}
     '''
     usda_database_check = requests.get(initial_search_request)
     check_json = usda_database_check.json()
-    total_number = check_json['list']['total']
+    current_total = check_json['list']['total']
     current_sr = check_json['list']['sr']
-    return {'total': total_number, 'current_sr': current_sr}
+    return {'current_total': current_total, 'current_sr': current_sr}
 
 
-def check_dbitem_changed(NDBNO_TOTAL, current_ndbno, LAST_SR,
-                         current_sr):  # can this be a wrapper?
+def check_dbitem_changed(LAST_NDBNO_TOTAL, current_ndbno_total, LAST_SR, current_sr):  # TODO: convert to decorator
     '''Returns True if Database has a different number of items
     '''
-    if NDBNO_TOTAL != current_ndbno:
+    if LAST_NDBNO_TOTAL != current_ndbno_total:
         return True
     elif LAST_SR != current_sr:
         return True
@@ -31,8 +24,7 @@ def check_dbitem_changed(NDBNO_TOTAL, current_ndbno, LAST_SR,
         return False
 
 
-def get_ndbno(current_ndbno, search_api_request, formt, q, sort, mx,
-              offset_temp, API_KEY):
+def get_ndbno_list(search_api_request, formt, q, sort, mx, offset, API_KEY):
     '''This returns a list of ndbno's in the usda foods database
     '''
     search_object = requests.get(search_api_request)
@@ -41,12 +33,12 @@ def get_ndbno(current_ndbno, search_api_request, formt, q, sort, mx,
     ndbno_list_dict = search_json['list'][
         'item']  # ndbno_list_dict is a list of dictionaries, where each dictionary is a unique food item
 
-    ndbno_list = []
-
-    # unpack ndbno_list_dict
-    for dictionary in ndbno_list_dict:
-        ndbno_list.append(dictionary['ndbno'])
-
-    return ndbno_list
+    return ndbno_list_dict
 
 
+def get_ndbno_full_report(report_api_request, ndbno_id):
+    '''Returns JSON Full Report
+    '''
+    full_report = requests.get(report_api_request)
+    full_report_json = full_report.json()
+    return full_report_json
