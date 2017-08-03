3a020ca63e3125ae75ab019b8a364e614965542b
jamiemenhall <jamiemenhall@mac.com>
2017-08-03 10:29:14 -0400

    updated init and setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(name='Distutils',
+      version='1.0',
+      description='Python Distribution Utilities',
+      author='Jamie Menhall',
+      author_email='jamiemenhall@mac.com',
+      url=' ',
+      packages=['distutils', 'distutils.command'],
+     )
